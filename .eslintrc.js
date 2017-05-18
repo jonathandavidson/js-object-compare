b7464b42f6c3e675761bfd8d00c7ef558444c2e4
Jonathan Davidson <jdavidson@netsplasher.com>
2017-05-18 09:52:25 -0500

    Install test and lint deps

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "extends": "standard",
+    "plugins": [
+        "standard",
+        "promise"
+    ],
+    "rules": {
+        "semi": 0
+    },
+    "globals": {
+        "beforeEach": true,
+        "describe": true,
+        "expect": true,
+        "it": true,
+        "jasmine": true
+    }
+};
